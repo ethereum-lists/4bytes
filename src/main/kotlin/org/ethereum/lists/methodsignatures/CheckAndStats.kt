18e500fa11bbd7d1e94f554b5f0f6013a1b97510
ligi <ligi@ligi.de>
2018-02-26 12:43:31 +0100

    Some more stats

--- src/main/kotlin/org/ethereum/lists/methodsignatures/CheckAndStats.kt
+++ src/main/kotlin/org/ethereum/lists/methodsignatures/CheckAndStats.kt
@@ -6,6 +6,10 @@ import org.walleth.khex.toNoPrefixHexString
 
 fun main(args: Array<String>) {
     var totalProcessed = 0
+    var withUnderscore = 0
+    var withCamelCase = 0
+    var maxParams = 0
+    var startsWiUpperCase = 0
     val store = FileBackedMethodSignatureStore(signatureDirectory)
     val methodSet = HashSet<String>()
     val paramSet = HashSet<String>()
@@ -16,6 +20,17 @@ fun main(args: Array<String>) {
             val methodName = signatureText.substringBefore("(")
             val params = signatureText.substringAfter("(").substringBefore(")").split(",").toHashSet()
             methodSet.add(methodName)
+            if (methodName.contains("_")) {
+                withUnderscore++
+            } else if (methodName != methodName.toLowerCase()) {
+                withCamelCase++
+            }
+            if (methodName.first().toLowerCase() != methodName.first()) {
+                startsWiUpperCase++
+            }
+            if (params.size > maxParams) {
+                maxParams = params.size
+            }
             val textHash = signatureText.toByteArray().keccak().toNoPrefixHexString()
             if (!textHash.startsWith(signatureHash)) {
                 error("signature fail $signatureHash $signatureText $textHash")
@@ -28,11 +43,15 @@ fun main(args: Array<String>) {
 
     println("totalProcessed: $totalProcessed")
     println("unique method names: ${methodSet.size}")
+    println("names with underscore: $withUnderscore")
+    println("names with CamelCase: $withCamelCase")
+    println("names starting with upper case char: $startsWiUpperCase")
     val max = methodSet.maxBy { it.length }?.length
     val min = methodSet.minBy { it.length }?.length
     println("min method name length: $min")
     println("max method name length: $max")
     val size = paramSet.size
+    println("max param count: $maxParams")
     println("param types used: $size")
     println("param types: ${paramSet.joinToString()}")
 
