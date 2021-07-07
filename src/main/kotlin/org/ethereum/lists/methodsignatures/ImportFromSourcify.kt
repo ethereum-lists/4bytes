3fde3a1b2e002736e9a5e0c35df5bbe386a18d55
ligi <ligi@ligi.de>
2021-07-07 06:44:58 +0200

    Care for Sourcify import script

--- src/main/kotlin/org/ethereum/lists/methodsignatures/ImportFromSourcify.kt
+++ src/main/kotlin/org/ethereum/lists/methodsignatures/ImportFromSourcify.kt
@@ -1,5 +1,7 @@
 package org.ethereum.lists.methodsignatures
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.kethereum.abi.getAllFunctions
 import org.kethereum.metadata.model.EthereumMetadataString
 import org.kethereum.metadata.parse
@@ -11,12 +13,11 @@ fun main() {
 }
 
 private fun import() {
-    // downloaded via `ipfs get /ipns/QmNmBr4tiXtwTrHKjyppUyAhW1FQZMJTdnUrksA9hapS4u`
-    File("verification.komputing/QmNmBr4tiXtwTrHKjyppUyAhW1FQZMJTdnUrksA9hapS4u/contract").listFiles()?.forEach { network ->
+    // downloaded via `ipfs get /ipns/k51qzi5uqu5dll0ocge71eudqnrgnogmbr37gsgl12uubsinphjoknl6bbi41p`
+    File("sourcify/k51qzi5uqu5dll0ocge71eudqnrgnogmbr37gsgl12uubsinphjoknl6bbi41p/contracts/full_match").listFiles()?.forEach { network ->
         network.listFiles()?.forEach {
             val json = File(it, "metadata.json").readText()
-
-            EthereumMetadataString(json).parse()?.output?.abi?.getAllFunctions()?.forEach { signature ->
+            EthereumMetadataString(json).parse(Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build())?.output?.abi?.getAllFunctions()?.forEach { signature ->
                 if (store.upsert(signature)) {
                     println("found new signature: " + signature.toTextMethodSignature().normalizedSignature)
                 }
