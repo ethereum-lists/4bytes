83fd7549505eb7c87b606be6943025c6959bb83a
ligi <ligi@ligi.de>
2021-12-11 17:52:07 +0100

    Add signatures from sourcify

--- src/main/kotlin/org/ethereum/lists/methodsignatures/ImportFromSourcify.kt
+++ src/main/kotlin/org/ethereum/lists/methodsignatures/ImportFromSourcify.kt
@@ -3,6 +3,8 @@ package org.ethereum.lists.methodsignatures
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.kethereum.abi.getAllFunctions
+import org.kethereum.contract.abi.types.isETHType
+import org.kethereum.contract.abi.types.model.NamedETHType
 import org.kethereum.metadata.model.EthereumMetadataString
 import org.kethereum.metadata.parse
 import org.kethereum.methodsignatures.toTextMethodSignature
@@ -14,14 +16,27 @@ fun main() {
 
 private fun import() {
     // downloaded via `ipfs get /ipns/k51qzi5uqu5dll0ocge71eudqnrgnogmbr37gsgl12uubsinphjoknl6bbi41p`
-    File("sourcify/k51qzi5uqu5dll0ocge71eudqnrgnogmbr37gsgl12uubsinphjoknl6bbi41p/contracts/full_match").listFiles()?.forEach { network ->
+    File("/home/ligi/tmp/sourcify3/sourcify_extract/data/contracts/full_match").listFiles()?.forEach { network ->
         network.listFiles()?.forEach {
             val json = File(it, "metadata.json").readText()
-            EthereumMetadataString(json).parse(Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build())?.output?.abi?.getAllFunctions()?.forEach { signature ->
-                if (store.upsert(signature)) {
-                    println("found new signature: " + signature.toTextMethodSignature().normalizedSignature)
-                }
+            try {
+                EthereumMetadataString(json).parse()?.output?.abi?.getAllFunctions()
+                    ?.forEach { signature ->
+                        signature.inputs.forEach {
+                            if (!NamedETHType(it.type.substringBefore("[")).isETHType()) {
+                                throw IllegalArgumentException("Contains non ETH type: " + it.type)
+                            }
+                        }
+                        signature.toTextMethodSignature()
+                        if (store.upsert(signature)) {
+                            println("found new signature: " + signature.toTextMethodSignature().normalizedSignature)
+                        }
 
+                    }
+            } catch (e: Exception) {
+                println("---")
+                println(e.message)
+                println(json)
             }
         }
     }
